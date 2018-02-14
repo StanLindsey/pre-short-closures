3800e33b7915cdf17447326961e5bdc58f37e074
Christopher Pitt <cgpitt@gmail.com>
2018-02-14 09:44:43 +0200

    Add a test for single expression bodies

--- tests/SemanticsTest.php
+++ tests/SemanticsTest.php
@@ -20,7 +20,27 @@ class SemanticsTest extends TestCase
 
             return $cb(3);
         ');
-        
-        $this->assertEquals(5, eval(substr($code, strlen('<?php'))), $code);
+
+        $this->assertEvaluatedCodeEquals($code, 5);
+    }
+
+    private function assertEvaluatedCodeEquals($code, $expected)
+    {
+        $this->assertEquals($expected, eval(substr($code, strlen('<?php'))), $code);
+    }
+
+    /**
+     * @test
+     */
+    public function can_process_single_expression_bodies()
+    {
+        Pre\Plugin\addMacro(__DIR__ . "/../source/macros.yay");
+
+        $code = Pre\Plugin\parse('<?php
+            $b = 2;
+            return ($a ⇒ $a + $b)(3);
+        ');
+
+        $this->assertEvaluatedCodeEquals($code, 5);
     }
 }
